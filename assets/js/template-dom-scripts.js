8693ce5fb714b63426f1103bf3d63eb8351a52b1
Tom Houlé <tom@tomhoule.com>
2022-05-08 11:57:17 +0200

    Delet dead code about expandable sections
    This is a cupper shortcode I am not planning to use.

--- assets/js/template-dom-scripts.js
+++ assets/js/template-dom-scripts.js
@@ -1,22 +1,3 @@
-/* Expandable sections */
-(function () {
-  function toggle (button, target) {
-    var expanded = button.getAttribute('aria-expanded') === 'true';
-    button.setAttribute('aria-expanded', !expanded);
-    target.hidden = !target.hidden;
-  }
-
-  var expanders = document.querySelectorAll('[data-expands]');
-
-  Array.prototype.forEach.call(expanders, function (expander) {
-    var target = document.getElementById(expander.getAttribute('data-expands'));
-
-    expander.addEventListener('click', function () {
-      toggle(expander, target);
-    })
-  })
-}());
-
 /* Menu button */
 (function () {
   var button = document.getElementById('menu-button');
